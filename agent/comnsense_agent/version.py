6690485ac9a5865501956e52289ee484bcfa3ac1
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2015-08-13 16:44:45 +0300

    TypeError: decoding Unicode is not supported

--- agent/comnsense_agent/version.py
+++ agent/comnsense_agent/version.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
-__product__ = u"comnsense-agent"
-__version__ = u"1.0.0.0"
-__author_email__ = u"team@comnsense.io"
-__author__ = u"Comnsense Team <%s>" % __author_email__
+__product__ = "comnsense-agent"
+__version__ = "1.0.0.0"
+__author_email__ = "team@comnsense.io"
+__author__ = "Comnsense Team <%s>" % __author_email__
 __copyright__ = u"© Comnsense, 2015"
