d6b090f5228c1fdda34d5c08b00edf45330be600
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2015-08-21 15:26:27 +0300

    agent refactoring

--- agent/comnsense_agent/agent.py
+++ agent/comnsense_agent/agent.py
@@ -1,11 +1,7 @@
 import logging
 import random
 import pickle
-import sys
 import time
-
-# import zmq
-# from zmq.eventloop import ioloop, zmqstream
 
 from comnsense_agent.serverstream import ServerStream
 from comnsense_agent.worker import create_new_worker
@@ -17,88 +13,76 @@ from comnsense_agent.socket import ZMQRouter, ZMQDealer
 logger = logging.getLogger(__name__)
 
 
-class Agent:
-    def __init__(self, bind_str, server_str):
-        self.bind_str = bind_str
-        self.server_str = server_str
-        logger.debug("server connection str: %s", server_str)
+class Agent(object):
+    def __init__(self, frontend_bind, server_address, loop):
+        self.frontend, _ = self.create_frontend_socket(loop, frontend_bind)
+        self.backend, self.backend_bind = self.create_backend_socket(loop)
+        self.client, _ = self.create_client_socket(loop, server_address)
 
-    def create_frontend_socket(self, loop):
+        self.frontend.on_recv(self.frontend_routine)
+        self.backend.on_recv(self.backend_routine)
+        self.client.on_recv(self.client_routine)
+
+        self.workers = {}
+        self.loop = loop
+
+    def create_frontend_socket(self, loop, bind_str):
         frontend = ZMQRouter()
-        frontend.bind(self.bind_str, loop)
-        logger.info("frontend socket bind: %s", self.bind_str)
-        return frontend
+        frontend.bind(bind_str, loop)
+        logger.info("frontend socket bind: %s", bind_str)
+        return frontend, bind_str
 
-    def setup_server(self, loop):
-        server_stream = ServerStream(self.server_str, loop)
-        return server_stream
+    def create_client_socket(self, loop, address):
+        client = ServerStream(address, loop)
+        logger.info("client connects to: %s", address)
+        return client, address
 
     def create_backend_socket(self, loop):
         backend = ZMQRouter()
         bind_str = backend.bind_unused_port(loop)
-        logger.debug("backend socket bind: %s", bind_str)
+        logger.info("backend socket bind: %s", bind_str)
         return backend, bind_str
 
-    def run(self, loop=None):
-        if loop is None:
-            loop = ioloop.IOLoop.instance()
+    def frontend_routine(self, msg):
+        if msg.is_event():
+            logger.debug("receive from addin: %s", msg)
 
-        server_stream = self.setup_server(loop)
-        frontend = self.create_frontend_socket(loop)
-        backend, backend_conn = self.create_backend_socket(loop)
+            if msg.ident not in self.workers or \
+                    not self.workers[msg.ident].is_alive():
+                worker = create_new_worker(msg.ident, self.backend_bind)
+                self.workers[msg.ident] = worker
+                # TODO redo this
+                time.sleep(3)
 
-        workers = {}
+            self.backend.send(msg)
 
-        def on_frontend_recv(msg):
-            logger.debug("frontend recv: %s", msg)
-            if msg.is_event() or msg.is_request() or msg.is_signal():
-                if msg.ident not in workers or \
-                        not workers[msg.ident].is_alive():
-                    worker = create_new_worker(msg.ident, backend_conn)
-                    workers[msg.ident] = worker
-                    # TODO redo this
-                    time.sleep(3)
-                if msg.is_signal():
-                    signal = Signal.deserialize(msg.payload)
-                    if signal.code == Signal.Code.Ready:
-                        logger.info("workbook %s is ready", msg.ident)
-                    else:
-                        logger.warn("unexpected signal: %s", signal)
-                else:
-                    backend.send(msg)
-            else:
-                logger.warn("unexpected message kind: %s", msg.kind)
+    def backend_routine(self, msg):
+        if msg.is_action():
+            logger.debug("send to addin: %s", msg)
+            self.frontend.send(msg)
 
-        def on_backend_recv(msg):  # receive answer from backend
-            if msg.is_action():
-                logger.debug("send to excel: %s", msg)
-                frontend.send(msg)  # send answer to excel
-            elif msg.is_request():  # send request to server
-                server_stream.send(msg)
-            elif msg.is_log():  # log from backend
-                logger.handle(pickle.loads(msg.payload))
-            elif msg.is_signal():
-                signal = Signal.deserialize(msg.payload)
-                if signal.code == Signal.Code.Ready:
-                    logger.info("worker with ident %s is ready", msg.ident)
-                else:
-                    logger.warn("unexpected signal: %s", signal)
-            else:
-                logger.warn("unexpected message kind: %s", msg.kind)
+        elif msg.is_request():
+            self.client.send(msg)
 
-        def on_server_recv(msg):
-            backend.send(msg)
+        elif msg.is_log():
+            logger.handle(pickle.loads(msg.payload))
 
-        frontend.on_recv(on_frontend_recv)
-        backend.on_recv(on_backend_recv)
-        server_stream.on_recv(on_server_recv)
+        elif msg.is_signal():
+            signal = Signal.deserialize(msg.payload)
+            if signal.code == Signal.Code.Ready:
+                logger.info("worker with ident %s is ready", msg.ident)
+
+    def client_routine(self, msg):
+        self.backend.send(msg)
+
+    def start(self):
         try:
-            loop.start()
+            self.loop.start()
         finally:
-            for worker in workers.values():
+            for worker in self.workers.values():
                 if worker.is_alive():
                     worker.kill()
                     worker.join()
 
-            frontend.close()
-            backend.close()
+            self.frontend.close()
+            self.backend.close()
