277cb907dfc9572c82ebafc3d9c8d421d7212816
Vladimir Almaev <almaev.vladimir@gmail.com>
2015-09-01 21:07:14 +0300

    Added Comnsense.ExcelAddin.UnitTests project

---
+++ comnsense/Tests/Comnsense.ExcelAddin.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Comnsense.ExcelAddin.UnitTests")]
+[assembly: AssemblyProduct("Comnsense.ExcelAddin.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © comnsense 2015")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4d1f2b66-6265-4d84-8160-a61146ba9f28")]
